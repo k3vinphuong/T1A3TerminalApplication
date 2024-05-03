efc2106586b4f83d63540720d5c974fb4fcffc83
k3vinphuong <kphuong230900@gmail.com>
2024-05-03 11:23:03 +1000

    tidied up coding

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-# System packages
-
 # External packages
 from colored import Fore, Back, Style
 
 # Imports from own functions
 from todo_functions import load_data,show_watchlist,add_movie,add_tv_series
 
+# Main screen menu with all the options
 def main():
     data = load_data()
     while True:
@@ -16,6 +15,7 @@ def main():
         print("4. Exit")
         choice = input("Enter your choice: ")
 
+        # Choices and the command to activate them
         if choice == "1":
             show_watchlist(data)
         elif choice == "2":
@@ -28,5 +28,7 @@ def main():
         else:
             print("Invalid choice. Please try again.")
 
+
+# Used to execute the code from main.py
 if __name__ == "__main__":
     main()
--- todo_functions.py
+++ todo_functions.py
@@ -1,16 +1,22 @@
+# Built in python feature used for this program
 import json
 
+# Defining load data allows the program to open a json file and see if theres data displayed and to create a file if there is not
 def load_data():
     try:
         with open("watchlist.json", "r") as file:
             return json.load(file)
+    # if file has not been created, gives it as just the movie and tv show and blank options
     except FileNotFoundError:
         return {"movies": [], "tv_series": []}
 
+# defines the save data option to allow all input movies/tv shows inputted to be saved into json file
 def save_data(data):
     with open("watchlist.json", "w") as file:
+        # Used to transfer pythhon objects into the json file
         json.dump(data, file, indent=4)
 
+# Shows the watchlist of what movies/tv shows that we have added
 def show_watchlist(data):
     print("Movies:")
     for idx, movie in enumerate(data["movies"], start=1):
@@ -19,6 +25,7 @@ def show_watchlist(data):
     for idx, series in enumerate(data["tv_series"], start=1):
         print(f"{idx}. {series['title']} - Rating: {series.get('rating', 'N/A')} - Completed: {series.get('completed', 'No')}")
 
+# Gives options to add the title, rating and whether we have completed the movie and then saves the data to the json file
 def add_movie(data):
     title = input("Enter movie title: ")
     rating = input("Enter rating (optional): ")
@@ -27,6 +34,7 @@ def add_movie(data):
     save_data(data)
     print("Movie added successfully!")
 
+# Does the same as the previous option but adds a tv show instead
 def add_tv_series(data):
     title = input("Enter TV series title: ")
     rating = input("Enter rating (optional): ")
